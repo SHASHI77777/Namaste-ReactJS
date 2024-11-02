3b9229b88b76ffc8f812db7147c60705bd7d2a9d
shashi <shashircciit07@gmail.com>
2024-11-02 19:47:08 +0530

    episode-01

---
+++ App.js
@@ -0,0 +1,42 @@
+// const heading = React.createElement(
+//   "h1",
+//   { id: "heading" },
+//   "hello  react js "
+// );
+
+// const root = ReactDOM.createRoot(document.getElementById("root"));
+// root.render(heading);
+
+const parent = React.createElement(
+    "div",
+    { id: "parent" },
+    [
+     React.createElement(
+     "div",
+       { id: "child" },
+        [React.createElement("h1", {},"i'm am h1 tag"),
+        React.createElement("h1", {},"i'm am h2 tag"),
+    ]),
+        React.createElement(
+        "div",
+          { id: "child2" },
+           [
+            React.createElement("h1", {},"i'm am h3 tag"),
+           React.createElement("h1", {},"i'm am h4 tag"),
+       ]),
+    ]);
+ 
+console.log(parent);
+
+const heading = React.createElement(
+ "h1", 
+{ id:"heading",xyz:"abc"}, 
+"Hello world from React!"
+);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(heading);
+
+ root.render(parent);
+
